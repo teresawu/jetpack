bbbf3f3fad259e87988ec635fcb5d3a575c6b661
Teresa <teresa.wu@ostmodern>
2018-10-30 22:40:28 +0000

    add lib

--- app/src/main/java/co/uk/jetpack/MainActivity.kt
+++ app/src/main/java/co/uk/jetpack/MainActivity.kt
@@ -1,12 +1,17 @@
 package co.uk.jetpack
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnCoroutine.setOnClickListener {
+//            startActivity(Intent(this@MainActivity, MainActivity::class.java))
+        }
     }
 }
